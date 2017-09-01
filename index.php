3177f37e30d2de4f72047e770a5558992432c558
Ignas Grigaravicius <kickeris@gmail.com>
2017-09-01 13:42:55 +0100

    Code split to header and footer

--- index.php
+++ index.php
@@ -1,40 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title></title>
-<link rel="stylesheet" type="text/css" href="reset.css">
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+<?php 
+	include_once 'header.php';
+ ?>
 
-<header>
-<nav>
-	<div class="main-wrapper">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-		</ul>
-		<div class="nav-login">
-			<form>
-				<input type="text" name="uid" placeholder="Username / email">
-				<input type="password" name="pwd" placeholder="password">
-				<button type="submit" name="submit">Login</button>
-			</form>
-			<a href="signup.php"> Sign up</a>
-		</div>
-	</div>
-</nav>
-
-</header>
-<section>		
-
-
+<section class="main-container">
+	<div class="main-wrapper">	
+		<h2>Home</h2>
+	</div>	
 </section>
-</body>
-</html>
 
-
-
-<!-- <?php
-	echo "Hello World!";
-?> -->
+<?php 
+	include_once 'footer.php';
+ ?>
